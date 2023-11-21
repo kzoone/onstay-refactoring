69894383226a7bb7e957d440d996f63800eeecbf
tneod2 <tneod2@icloud.com>
2023-11-22 03:39:29 +0900

    [Design] header 작업
    - public 폴더에 로고 이미지 추가 (logo.png)
    - App.jsx 파일에 _home.scss 링크 import
    - Header.jsx 수정
    - _home.scss에서 헤더 부분 스타일 작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './style/pages/_home.scss';
 import { Outlet } from 'react-router-dom';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,5 +1,26 @@
+import { Link } from 'react-router-dom';
+import { FaRegUser } from "react-icons/fa6";
+
 export default function Header() {
     return(
-        <div>header</div>
+        <div className='header'>
+            <div className='header_left'>
+                <Link to='/'>
+                    <img className='header_logo' src='logo.png' />
+                </Link>
+            </div>
+            <div className='header_right'>
+                <nav>
+                    <Link className='menu' to='/findstay'>FIND STAY</Link>
+                    <Link className='menu' to='/newstay'>NEW-STAY</Link>
+                    <Link className='menu' to='#'>JOURNAL</Link>
+                    <Link className='menu' to='/notice'>NOTICE</Link>
+                </nav>
+                <div className='my'>
+                    <Link className='mypage' to='#'><FaRegUser /> MY PAGE</Link>
+                    <Link className='logout' to='#'>LOGOUT</Link>
+                </div>
+            </div>
+        </div>
     );
 }
