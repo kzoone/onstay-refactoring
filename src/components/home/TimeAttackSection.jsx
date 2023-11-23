5f2c5133794a1ad1e2c647a7fe335a048487e4e7
DESKTOP-H6B44QU\TJ <yhoonsong96@gmail.com>
2023-11-23 16:37:12 +0900

    [Feat] Home - TimeAttackSection UI 디자인 및 기능 구현 완료
    - TimeAttackSection 반응형 디자인 구현 완료
    - 익일 오전 0시까지 남은 시간 타이머 기능 구현
    - 매일 오전 0시에 타이머가 초기화되며 새로운 숙소를 렌더링

--- src/components/home/TimeAttackSection.jsx
+++ src/components/home/TimeAttackSection.jsx
@@ -1,25 +1,50 @@
+import { useEffect, useState } from "react";
 import { LuAlarmClock } from "react-icons/lu";
+import { Link } from 'react-router-dom';
+import Timer from './Timer';
+import axios from "axios";
 
 export default function TimeAttackSection() {
+  const now = new Date()
+  let [day, setDay] = useState(now.getDate())
+
+  const [acc, setAcc] = useState({acc_id : null, acc_name : null, acc_image : null})
+
+  // 숙소 아이디 형식으로 변환하는 함수 (3이면 "AC0003"으로 변환)
+  const accIdFormat = num => {
+    return "AC" + "0".repeat(4-String(num).length) + num
+  }
+
+  // 현재 날짜에 해당하는 숙소id 정보를 가져옴 (예를들어 3일이면 "AC0003" 숙소가 이벤트 숙소인것)
+  // 익일 0시가 되어, 날짜가 달라지면 숙소 정보를 새로운 숙소로 리렌더링하도록 함
+  useEffect(()=>{
+    axios.get('http://localhost:8000/acc/' + accIdFormat(day))
+    .then(res=>{
+      const {acc_id, acc_name} = res.data.acc_info
+      const acc_image = res.data.acc_images.filter(image=>image.img_size==='big')[0]?.acc_img
+      setAcc({acc_id, acc_name, acc_image})
+    })
+  },[day])
+  
   return(
     <section className='home_timeattack_section'>
       <div className='timeattack_container'>
         <div className='timeattack_text'>
           <h4>TIME ATTACK</h4>
-          <span className='acc_name'>숙소 이름</span>
+          <span className='acc_name'>{acc.acc_name}</span>
           <span className='timeattack_desc'>
-            오늘 하루 특가 예약 이벤트를 진행중인 한옥 숙소에요.      
-            가성비&가심비 모두 잡을 기회를 놓치지 마세요 !
+            오늘 하루 특가 예약 이벤트를 진행중인 한옥 숙소에요.
+            가성비•가심비 모두 잡을 기회를 놓치지 마세요 !
           </span>
           <small>할인 종료까지</small>
           <div className='timeattack_time'>
             <LuAlarmClock/>
-            <span>21시간 14분 9초 남았어요!</span>
+            <Timer setDay={setDay}/>
           </div>
         </div>
-        <div className='timeattack_img'> 
-          <img src='assets/images/swiper/swiperImage1.webp' alt="" />
-        </div>
+        <Link to={`/findstay/acc/${acc.acc_id}`} className='timeattack_img'> 
+          <img src={`assets/images/swiper/${acc.acc_image}`} alt="" />
+        </Link> 
       </div>
     </section>
   );
