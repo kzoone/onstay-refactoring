849f5b10cee2d48da808eb91010615f51216a873
jeongyumin <onewmean@gmail.com>
2023-11-23 17:28:24 +0900

    [Remove] .gitkeep 및 CategoryTitle 컴포넌트 삭제
    - 컴포넌트 구조 수정으로 CategoryTitle 컴포넌트 삭제
      CategorySwiper 컴포넌트로 코드 추가

--- src/components/home/categorysection/CategoryTitle.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function CategoryTitle({type}){
-  return(
-    <>
-    { type === 'only' ? <p className="title">오직 온스테이하우스에서만</p> :  <p className="title">20만원 이하 가성비 하우스</p> }
-    </>
-  );
-}
