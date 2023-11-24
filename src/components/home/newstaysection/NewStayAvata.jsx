2cfb6f52b00606aabdae46dde89223778662d728
yunjeong <yjleeeeee6@gmail.com>
2023-11-24 16:57:15 +0900

    [Add] NewStay 숙소 이미지 및 개요 출력
    - db에서 받아온 데이터 출력
    - 지역명 코드 추가

---
+++ src/components/home/newstaysection/NewStayAvata.jsx
@@ -0,0 +1,7 @@
+export default function NewStayAvata({acc_img}) {
+  return (
+      <figure>
+        <img src={`/assets/images/acc/${acc_img}`} alt='New Stay' />
+      </figure>
+  );
+};
--- src/components/home/newstaysection/NewStaySection.jsx
+++ src/components/home/newstaysection/NewStaySection.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import NewStayAvata from './NewStayAvata';
+import { SlArrowRight } from "react-icons/sl";
+import NewStayContent from './NewStayContent';
 
 export default function NewStaySection() {
   const [newStayList, setNewStayList] = useState([])
@@ -8,16 +11,16 @@ export default function NewStaySection() {
   /**
    * 무작위 배열
    * @param {*} array 3개월 이내 등록된 숙소 데이터
-   * @param {*} count 원하는 길이의 값
-   * @returns 무작위 배열의 길이 6까지 리턴
+   * @param {*} count 추출을 종료 할 0 기준 인덱스
+   * @returns 무작위 배열에서 0 ~ (count - 1) 인덱스까지 추출
    */
   function shuffle(array, count) {
     for (let index = array.length - 1; index >= 0; index--) {
       let random = Math.floor(Math.random() * (index + 1));
       [array[index], array[random]] = [array[random], array[index]];
     }
-    const selectedElements = array.slice(0, count);
-    return selectedElements;
+    const selectedArr = array.slice(0, count);
+    return selectedArr;
   }
 
   useEffect(() => {
@@ -30,8 +33,21 @@ export default function NewStaySection() {
 
   return (
     <div className='newstaysection'>
-      <p>NEW STAY</p>
-      <Link to='/newstay'>전체보기 &gt;</Link>
+      <div>
+        <p>NEW STAY</p>
+        <Link to='/newstay'>전체보기 <SlArrowRight /></Link>
+      </div>
+      <ul>
+        {newStayList.map(newStay =>
+          <li key={newStay.acc_img}>
+            <Link to={`/findstay/acc/${newStay.acc_id}`}>
+              <NewStayAvata acc_img={newStay.acc_img} />
+              <NewStayContent acc_name={newStay.acc_name} area_code={newStay.area_code}
+                acc_summary1={newStay.acc_summary1} />
+            </Link>
+          </li>
+        )}
+      </ul>
     </div>
   );
 };
