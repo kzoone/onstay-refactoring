b946c6ccd2dbc80ee1aca79660d2db3719d6260c
jeongyumin <onewmean@gmail.com>
2023-11-25 16:30:39 +0900

    [ Add ] roomdetail 페이지 컴포넌트 ui 및 코드 구현 (ing)
    - RoomDetail.jsx
      페이지 안 컴포넌트 호출
    
    - RoomContent.jsx, ContentWrap.jsx, RoomInfo.jsx
      RoomSwiper.jsx, CheckDate.jsx, OtherRoom.jsx
      AdditionalList.jsx, Additional.jsx
    
      axios 데이터 요청으로 해당 하는 객실 ui 구현 진행 ing
      ( 기본 페이지 1차 구현 및 otherroom 링크 이동 렌더링 구현 )
    
    - main.scss, _roomdetail.scss
      scss 경로 추가 및 코드 위치 수정, 세미콜론 수정
    
    - .gitkeep 삭제

---
+++ src/components/roomdetail/Additional.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Additional({str, type, title}) {
+  const features = ['빅테이블', '정원', '테라스', '독립 키친', '독립 화장실', '산책로', '샤워실', 'BBQ'];
+
+  return (
+    <>
+      <div className={`${type}_container common_container`}>
+        <p className='title'>{title}</p>
+        <div className='info'>
+          { type === 'features' ? 
+            ( str.map((code, idx) => (
+              code ? (
+                <div key={idx}>
+                  <img src={`/assets/images/features/${parseInt(code)}.png`} alt="" />
+                  <p className="features">{features[parseInt(code - 1)]}</p>
+                </div>
+                ) : null
+              ))) 
+            : ( str.map(p => ( <p className="amenities">{p}</p> )))
+          } 
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/roomdetail/RoomContent.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ContentWrap from './ContentWrap';
+import RoomInfo from './RoomInfo';
+import RoomSwiper from './RoomSwiper';
+import CheckDate from './CheckDate';
+import AdditionalList from './AdditionalList';
+import OtherRoom from './OtherRoom';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+export default function RoomContent() {
+  const [ roomContent, setRoomContent ] = useState({});
+  const [ otherContent, setOtherContent ] = useState({});
+  let { roomid } = useParams();
+
+  useEffect(() => {
+    axios.get(`http://127.0.0.1:8000/room/${roomid}`)
+    .then(result => {
+      if(result.data[0].room_id === roomid) {
+        setRoomContent(result.data[0]);
+        setOtherContent(result.data[1]);
+      } else {
+        setRoomContent(result.data[1]);
+        setOtherContent(result.data[0]);
+      }
+    })
+    .catch(error => console.log(error));
+  }, [roomid])
+  
+
+  return(
+    <ContentWrap>
+      <p className='acc_name'>{roomContent.acc_name}</p>
+      <div className='content_container'>
+        <RoomInfo data={roomContent}/>
+        <div className='room_swiper_container'>
+        <RoomSwiper img1={roomContent.room_img1} img2={roomContent.room_img2} img3={roomContent.room_img3} name={roomContent.room_name} acc={roomContent.acc_name} />
+        <CheckDate />
+        </div>
+      </div>
+      <AdditionalList featureCodes={roomContent.feature_codes} amenities={roomContent.amenities}/>
+      <OtherRoom data={otherContent} />
+    </ContentWrap>
+  );
+}
