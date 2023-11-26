f0f94628d25f3cea82f4c865716884c2ce2ac4bc
YHSONG\yhoon <yhoonsong96@gmail.com>
2023-11-27 02:27:28 +0900

    [Add] 회원가입 기능 개발 중
    - 회원가입 유효성 체크 state 기능 구현 완성
    - 중복 체크 코드 작성 완료 (테스트 필요)
    - src/constatns 폴더 및 js 파일 생성 후 회원가입 관련 상수 관리

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -2,111 +2,135 @@ import { Link } from 'react-router-dom';
 import PageTitle from './../components/common/PageTitle';
 import { useEffect, useRef, useState } from 'react';
 import JoinTerm from '../components/join/JoinTerm';
+import { MEMBER_REGEX, INVALID_NOTI_ALERT, INVALID_NOTI_TEXT, UNKNOWN_ERROR_ALERT } from '../constants/constants';
+import axios from 'axios';
 
 
 export function Join() {
-    const [form, setForm] = useState({user_id : '', user_email : '', 
-                                      user_name : '', user_pw : '', user_pw_repeat : '', phone : ''})
-    const [valid,setValid] = useState({user_id:false, user_id_duplicate : false, user_email:false, 
-                                       user_name:false, user_pw:false, user_pw_repeat:false, phone:false,
-                                       terms_required : false})
-    const refInvalid = useRef(null);
-
-    const idRegex = /^[A-Za-z\d]{6,20}$/
-    const emailRegex = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
-    const nameRegex = /^[ A-Za-z가-힣]{1,30}$/;
-    const pwRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{6,18}$/;
-    const phoneRegex = /^01[016789]\d{7,8}$/;
+    const [form, setForm] = useState({
+        user_id: '', user_email: '',
+        user_name: '', user_pw: '', user_pw_repeat: '', phone: ''
+    }) // submit 시 전송할 폼 데이터를 관리하는 state
+    const [valid, setValid] = useState({
+        user_id: false, user_id_unique: false, user_email: false,
+        user_name: false, user_pw: false, user_pw_repeat: false, phone: false,
+        terms_required: false
+    }) // submit 시, 회원가입이 가능한지 체크하는 항목들을 관리하는 state
+ 
+    const refElement = useRef(null);
 
     const handleChange = (e) => {
-        setForm({...form, [e.target.name]:e.target.value})
-        console.log(valid);
-    }
+        setForm({ ...form, [e.target.name]: e.target.value })
+        if (e.target.name === 'user_id') {
+            setValid({...valid, user_id_unique:false})
+        }
+    } // input change 이벤트
 
-    useEffect(()=>{
+    const userIdDuplicateCheck = () => {
+        if (!valid.user_id) return alert(INVALID_NOTI_ALERT.user_id) // 아이디 형식 안맞으면 중지
+        
+        axios.get('http://localhost:8000/member/duplication/' + form.user_id)
+        .then(res => {
+            let {isUnique} = res.data
+            if (isUnique) {
+                setValid({...valid, user_id_unique : true})
+            } else {
+                setValid({...valid, user_id_unique : false})
+                alert('중복된 아이디입니다. 다른 아이디를 입력해주세요.')
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            alert(UNKNOWN_ERROR_ALERT)
+        })
+    } // 아이디 중복체크
+
+    useEffect(() => {
         setValid({
             ...valid,
-            user_id : idRegex.test(form.user_id),
-            user_email : emailRegex.test(form.user_email),
-            user_name : nameRegex.test(form.user_name),
-            user_pw : pwRegex.test(form.user_pw),
-            user_pw_repeat : form.user_pw === form.user_pw_repeat,
-            phone : phoneRegex.test(form.phone),
+            user_id: MEMBER_REGEX.user_id.test(form.user_id),
+            user_email: MEMBER_REGEX.user_email.test(form.user_email),
+            user_name: MEMBER_REGEX.user_name.test(form.user_name),
+            user_pw: MEMBER_REGEX.user_pw.test(form.user_pw),
+            user_pw_repeat: form.user_pw === form.user_pw_repeat,
+            phone: MEMBER_REGEX.phone.test(form.phone),
         })
-    },[form])
-    
+    }, [form]) // form 내용이 업데이트 될때마다 각 valid 항목들을 업데이트 해줌
+
     const handleSubmit = (e) => {
         e.preventDefault()
-        let invalidNotiMsg = '폼 작성 내용을 다시 확인해주세요.';
-        const notiMsgs = {
-            user_id:'아이디를 형식에 맞게 입력해주세요.', 
-            user_id_duplicate : '아이디 중복체크를 진행해주세요.', 
-            user_email:'이메일을 형식에 맞게 입력해주세요.', 
-            user_name:'이름을 형식에 맞게 입력해주세요.', 
-            user_pw:'비밀번호를 형식에 맞게 입력해주세요.', 
-            user_pw_repeat:'동일한 비밀번호를 입력해주세요.', 
-            phone:'휴대전화번호를 형식에 맞게 입력해주세요.',
-            terms_required : '필수 동의 약관에 모두 체크해주세요.'
-        }
-        for (let id of Object.keys(valid)) {
-            if (!valid[id]) {
-                refInvalid.current = document.getElementById(id)
-                invalidNotiMsg = notiMsgs[id];
-                break;
+        
+        // valid 객체 안의 항목들이 모두 true인지 체크하여, 회원 가입이 가능한지 여부를 반환
+        let isJoinPossible = Object.values(valid).every(checkType=>checkType===true)
+
+        // 회원가입 불가능한 경우 (valid 안의 항목 중 하나라도 false인 경우)
+        if (!isJoinPossible) {
+            let invalidNotiMsg = '폼 작성 내용을 다시 확인해주세요.';
+            for (let checkType of Object.keys(valid)) {
+                if (!valid[checkType]) {
+                    refElement.current = document.getElementById(checkType)
+                    invalidNotiMsg = INVALID_NOTI_ALERT[checkType];
+                    break;
+                }
             }
+            try {refElement.current.focus()} // 참조 엘리먼트가 input인 경우
+            catch {refElement.current.scrollIntoView({ behavior: 'smooth' })} // 참조 엘리먼트가 input이 아닌 경우
+            return alert(invalidNotiMsg)
         }
-        try {
-            refInvalid.current.focus()
-        } catch{ 
-            refInvalid.current.scrollIntoView({behavior:'smooth'})
-        }
-        return alert(invalidNotiMsg)
+        
+       // 회원가입 가능한 경우 (valid 안의 항목들이 모두 true)
+       let {user_id, user_email, user_name, user_pw, phone} = form
+    //    axios({
+    //     url : 'http://localhost:8000/',
+    //     method : 'post',
+    //     data : {user_id, user_email, user_name, user_pw, phone}
+    //    })
     }
 
     return (
         <main className="member join">
-            <PageTitle title={'JOIN'} subtitle={'회원가입'}/>
+            <PageTitle title={'JOIN'} subtitle={'회원가입'} />
 
             <form onSubmit={handleSubmit} className="join_form member_form" action="">
                 <p>
                     <label htmlFor="user_id">아이디</label>
-                    <input onChange={(e)=>{handleChange(e)}} value={form.user_id} type="text" name='user_id' id='user_id' placeholder='아이디를 입력하세요' />
+                    <input onChange={(e) => { handleChange(e) }} value={form.user_id} type="text" name='user_id' id='user_id' placeholder='아이디를 입력하세요' />
                     {(!valid.user_id && form.user_id !== '') &&
-                    <small className="invalid_noti user_id">6~15자의 영문 및 숫자 조합만 가능합니다.</small>}
-                    {(valid.user_id && valid.user_id_duplicate) &&
-                    <small className="valid_noti user_id">사용 가능한 아이디입니다.</small>}
-                    <button id='user_id_duplicate' className='duplicate_check_btn white_box' type="button">중복체크</button>
+                        <small className="invalid_noti user_id">{INVALID_NOTI_TEXT.user_id}</small>}
+                    {(valid.user_id && valid.user_id_unique) &&
+                        <small className="valid_noti user_id">사용 가능한 아이디입니다.</small>}
+                    <button id='user_id_unique' onClick={userIdDuplicateCheck} className='duplicate_check_btn white_box' type="button">중복체크</button>
                 </p>
                 <p>
                     <label htmlFor="user_email">이메일</label>
-                    <input onChange={(e)=>{handleChange(e)}} value={form.user_email} type="text" name="user_email" id="user_email" placeholder="@까지 정확하게 입력하세요" />
-                    {(!valid.user_email && form.user_email!=='') &&
-                    <small className="invalid_noti user_email">이메일 형식이 올바르지 않습니다</small>}
+                    <input onChange={(e) => { handleChange(e) }} value={form.user_email} type="text" name="user_email" id="user_email" placeholder="@까지 정확하게 입력하세요" />
+                    {(!valid.user_email && form.user_email !== '') &&
+                        <small className="invalid_noti user_email">{INVALID_NOTI_TEXT.user_email}</small>}
                 </p>
                 <p>
                     <label htmlFor="user_name">이름</label>
-                    <input onChange={(e)=>{handleChange(e)}} value={form.user_name} type="text" name="user_name" id="user_name" placeholder="이용자 본인의 이름을 입력하세요" />
+                    <input onChange={(e) => { handleChange(e) }} value={form.user_name} type="text" name="user_name" id="user_name" placeholder="이용자 본인의 이름을 입력하세요" />
                     {(!valid.user_name && form.user_name !== '') &&
-                    <small className="invalid_noti user_name">1자 이상 30자 이하의 한글 및 영문으로 입력해주세요</small>}
+                        <small className="invalid_noti user_name">{INVALID_NOTI_TEXT.user_name}</small>}
                 </p>
                 <p>
                     <label htmlFor="user_pw">비밀번호</label>
-                    <input onChange={(e)=>{handleChange(e)}} value={form.user_pw} type="password" name="user_pw" id="user_pw" placeholder="비밀번호를 입력하세요" />
-                    {(!valid.user_pw && form.user_pw!=='') &&
-                    <small className="invalid_noti user_pw">영문, 숫자, 특수문자를 모두 포함하여 6~18자로 입력해주세요.</small>}
-                    <input onChange={(e)=>{handleChange(e)}} value={form.user_pw_repeat} type="password" name="user_pw_repeat" id="user_pw_repeat" placeholder="비밀번호를 한번 더 입력하세요." />
-                    {(!valid.user_pw_repeat && form.user_pw_repeat!=='') &&
-                      <small className="invalid_noti user_pw_repeat">비밀번호가 일치하지 않습니다.</small>}
+                    <input onChange={(e) => { handleChange(e) }} value={form.user_pw} type="password" name="user_pw" id="user_pw" placeholder="비밀번호를 입력하세요" />
+                    {(!valid.user_pw && form.user_pw !== '') &&
+                        <small className="invalid_noti user_pw">{INVALID_NOTI_TEXT.user_pw}</small>}
+                    <input onChange={(e) => { handleChange(e) }} value={form.user_pw_repeat} type="password" name="user_pw_repeat" id="user_pw_repeat" placeholder="비밀번호를 한번 더 입력하세요." />
+                    {(!valid.user_pw_repeat && form.user_pw_repeat !== '') &&
+                        <small className="invalid_noti user_pw_repeat">{INVALID_NOTI_TEXT.user_pw_repeat}</small>}
                 </p>
                 <p>
                     <label htmlFor="phone">휴대전화번호</label>
-                    <input onChange={(e)=>{handleChange(e)}} value={form.phone} type="text" name="phone" id="phone" placeholder="- 없이 휴대전화 번호를 입력하세요" />
+                    <input onChange={(e) => { handleChange(e) }} value={form.phone} type="text" name="phone" id="phone" placeholder="- 없이 휴대전화 번호를 입력하세요" />
                     {(!valid.phone && form.phone !== '') &&
-                    <small className="invalid_noti user_phone">휴대전화 형식이 올바르지 않습니다</small>
+                        <small className="invalid_noti phone">{INVALID_NOTI_TEXT.phone}</small>
                     }
                 </p>
 
-                <JoinTerm id="terms_required" setValid={setValid}/> 
+                <JoinTerm setValid={setValid} />
 
                 <button className="join_btn black_box">회원가입</button>
                 <Link to='/login' className="login_link white_box">로그인</Link>
