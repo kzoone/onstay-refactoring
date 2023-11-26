38e4d903e483c40cc1a9302e0bf6f870f058fcbc
yunjeong <yjleeeeee6@gmail.com>
2023-11-27 01:02:54 +0900

    [Add] TopButton 구현
    - - 스크롤의 길이에 따라 state값을 true나 false로 반환하여,
    showButton &&을 통해 값에 따라 조건 실행

---
+++ src/components/common/TopButton.jsx
@@ -0,0 +1,39 @@
+import { SlArrowUp } from 'react-icons/sl';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+export default function TopButton() {
+  const [showButton, setShowButton] = useState(false);
+
+  // window.scrollTo({top,left,behavior}) 최상단으로 이동
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  useEffect(() => {
+    // 스크롤의 길이에 따라 state값을 true나 false로 반환
+    const handleShowButton = () => {
+      if (window.scrollY > 500) {
+        setShowButton(true)
+      } else {
+        setShowButton(false)
+      }
+    };
+    
+    window.addEventListener('scroll', handleShowButton);
+
+    // clean-up function
+    return () => {
+      window.removeEventListener('scroll', handleShowButton);
+    };
+  }, []);
+
+  return showButton && (
+    <div className='top_btn'>
+      <button type='button' onClick={handleScrollToTop}><SlArrowUp /></button>
+    </div>
+  );
+};
