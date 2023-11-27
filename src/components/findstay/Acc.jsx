8bd1a3c785297b9469584d992b5c0c773d1e5b14
최유진 <tneod2@icloud.com>
2023-11-28 00:05:04 +0900

    [Add] 숙소 리스트 스와이퍼 기능 추가

--- src/components/findstay/Acc.jsx
+++ src/components/findstay/Acc.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
 
 export default function Acc({acc}){
     /**
@@ -26,6 +28,25 @@ export default function Acc({acc}){
         return codeinfo[area_code];
     };
 
+     // Swiper 컴포넌트의 ref를 생성
+    const swiperRef = useRef(null);
+
+    // 브라우저 리사이즈 이벤트 처리
+    const handleResize = () => {
+        // 스와이퍼 업데이트
+        if (swiperRef.current) {
+        swiperRef.current.swiper.update();
+        }
+    };
+
+    // 컴포넌트가 처음 마운트될 때와 언마운트될 때 리사이즈 이벤트 핸들러 등록 및 해제
+    React.useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+        window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return(
         <div className="acc">
             <div className="acc_title">{acc.acc_name}</div>
@@ -37,9 +58,18 @@ export default function Acc({acc}){
                     <p>예약하기</p>
                 </div>
                 <div className="acc_imgs">
-                    {acc.acc_img.split(',').filter(img => !img.startsWith('swiperImage')).map((img, index) => (
-                        <img key={index} src={`assets/images/acc/${img.trim()}`} />
-                    ))}
+                    <Swiper 
+                    navigation={true} 
+                    modules={[Navigation]}
+                    onResize={() => handleResize()}
+                    ref={swiperRef}
+                    >
+                        {acc.acc_img.split(',').filter(img => !img.startsWith('swiperImage')).map((img, index) => (
+                            <SwiperSlide>
+                                <img key={index} src={`assets/images/acc/${img.trim()}`} />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
                 </div>
             </div>
         </div>
