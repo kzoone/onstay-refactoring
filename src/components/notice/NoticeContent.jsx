a0b1de258cf1f4f8a6b19f583f2dbcb3a7640b8d
yunjeong <yjleeeeee6@gmail.com>
2023-11-30 02:39:22 +0900

    [Add] 페이지네이션 구현
    - react-js-pagination 라이브러리 사용
    - 한 페이지당 pageItem 수 , total_rows 데이터 요청

--- src/components/notice/NoticeContent.jsx
+++ src/components/notice/NoticeContent.jsx
@@ -3,25 +3,25 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import PagiNation from 'react-js-pagination';
-import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 
 export default function NoticeContent() {
   const [noticeList, setNoticeList] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
   const [page, setPage] = useState(1);
+  const noticePage = { page: page, pageItem: 5 }
 
   useEffect(() => {
-    axios.get('http://localhost:8000/notice/')
+    axios.post('http://localhost:8000/notice/', noticePage)
       .then(result => {
-        console.log(result.data);
         setNoticeList(result.data);
+        setTotalCount(result.data[0].total_rows);
       }).catch(error => console.log(error))
-  }, []);
+  }, [page]);
 
-  const handleChange = () => {
+  // 페이지 변경을 처리하는 함수
+  const handleChange = (page) => {
     setPage(page);
   }
-
-  console.log(noticeList.length);
 
   return (
     <>
@@ -34,26 +34,26 @@ export default function NoticeContent() {
             <th>VIEW COUNT</th>
           </tr>
         </thead>
-        {noticeList.map(notice =>
-          <tbody key={notice.notice_id}>
-            <tr>
+        <tbody>
+          {noticeList.map(notice =>
+            <tr key={notice.notice_id}>
               <th>{notice.no}</th>
               <td>
-                <Link to='/notice/1'>{notice.notice_title}</Link>
+                <Link to={`/notice/${notice.notice_id}`}>{notice.notice_title}</Link>
               </td>
               <td>{notice.notice_date}</td>
               <td>{notice.notice_views}</td>
-            </tr>
-          </tbody>)}
+            </tr>)}
+        </tbody>
       </table>
       <PagiNation
-      activePage={1}
-      itemsCountPerPage={5}
-      totalItemsCount={noticeList.length}
-      pageRangeDisplayed={1}
-      prevPageText={<SlArrowLeft />}
-      nextPageText={<SlArrowRight />}
-      onChange={handleChange}
+        activePage={page} // 현재 페이지
+        itemsCountPerPage={noticePage.pageItem} // 현재 페이지당 보여줄 아이템 개수
+        totalItemsCount={totalCount} // 총 아이템 개수
+        pageRangeDisplayed={5} // 보여줄 페이지 범위
+        prevPageText={'<'} // 이전을 나타낼 텍스트
+        nextPageText={'>'} // 다음을 나타낼 텍스트
+        onChange={handleChange} // 함수 호출
       />
     </>
   );
