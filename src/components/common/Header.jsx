39fe28686ab516bc0ba652aed39c19cf1388a5a3
tneod2 <tneod2@icloud.com>
2023-12-01 01:12:09 +0900

    [Add] 헤더 수정
    - z-index 추가
    - menubar_list에 open 클래스가 추가되어있는경우 배경 어둡게,
    - 스크롤 바 사라지게 함
    - menubar_list 외의 영역 클릭 시 닫힘
    - 회원로그인시, 관리자로그인시, 비로그인시 UI 다르게 만들었음

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FaRegUser } from "react-icons/fa6";
 import { SlMagnifier } from "react-icons/sl";
@@ -6,9 +7,38 @@ import { IoBookOutline } from "react-icons/io5";
 import { SlBell } from "react-icons/sl";
 import { SlMenu } from "react-icons/sl";
 import { TfiClose } from "react-icons/tfi";
+import { LuLogIn } from "react-icons/lu";
 import $ from 'jquery';
 
 export default function Header() {
+
+    /* menubar 제어 */
+    function menubarOpen(){ //menubar 버튼을 누르면 menubar_list를 OPEN
+            document.querySelector('.menubar_list').classList.add('open');
+    }
+    function menubarClose(){    //close 버튼을 누르면 menubar_list를 CLOSE
+            document.querySelector('.menubar_list').classList.remove('open');
+    }
+
+    document.addEventListener('click', (e) => {  // 문서 전체를 클릭 이벤트로 감지시킴
+
+        const isMenubarListArea = document.querySelector('.menubar_list').contains(e.target);
+        const isMenubarBtnArea = document.querySelector('.menubar').contains(e.target);
+        
+        if (!isMenubarListArea && !isMenubarBtnArea) { //menubar_list 영역 외부를 클릭한 경우에만 menubarClose 함수가 실행
+            menubarClose();
+        }
+        
+        //menubar_list가 open되었을때 배경 어둡게, 스크롤바 사라짐
+        if(document.querySelector('.menubar_list').classList.contains('open')){
+            document.querySelector('.menubar_bg').style.display = 'block';
+            document.querySelector('body').style.overflow = 'hidden';
+        }else{
+            document.querySelector('.menubar_bg').style.display = 'none';
+            document.querySelector('body').style.overflow = 'scroll';
+        }
+    })
+
 
     /* LOGOUT 버튼 눌렀을 때 */
     const handleLogout = () => {
@@ -31,14 +61,18 @@ export default function Header() {
 
             /* menubar가 open된 상태로 화면높이가 줄어들때
             로그아웃 버튼의 높이위치도 같이 줄어들다가 특정 위치까지 올라오면 고정시키는코드 */
-            let winH = window.innerHeight; //브라우저 높이 (변동)
-            let logoutAreaOffset = $('.logout_area').offset().top; 
-            let logoutOffset = $('.menubar_list .logout').offset().top;
-            let logoutH = document.querySelector('.menubar_list .logout').offsetHeight; 
-            if(logoutOffset <= logoutAreaOffset){
-                $('.menubar_list .logout').css({'bottom': `${winH-logoutAreaOffset-logoutH}px`});
-            }else if(logoutOffset > logoutAreaOffset){
-                $('.menubar_list .logout').css({'bottom': '80px'});
+            if ($('.logout_area').length > 0) {
+                let winH = window.innerHeight; //브라우저 높이 (변동)
+                let logoutAreaOffset = $('.logout_area').offset().top; 
+                let logoutOffset = $('.menubar_list .logout').offset().top;
+                let logoutH = document.querySelector('.menubar_list .logout').offsetHeight; 
+                if(logoutAreaOffset && logoutOffset){
+                    if(logoutOffset <= logoutAreaOffset && logoutH){
+                        $('.menubar_list .logout').css({'bottom': `${winH-logoutAreaOffset-logoutH}px`});
+                    }else if(logoutOffset > logoutAreaOffset){
+                        $('.menubar_list .logout').css({'bottom': '80px'});
+                    }
+                }
             }
         }
         scrollMotion()
@@ -48,15 +82,6 @@ export default function Header() {
             scrollMotion()
         })
     })
-
-    /* menubar 버튼을 누르면 menubar_list를 OPEN */
-    function menubarOpen(){
-            document.querySelector('.menubar_list').classList.add('open');
-    }
-    /* close 버튼을 누르면 menubar_list를 CLOSE */
-    function menubarClose(){
-            document.querySelector('.menubar_list').classList.remove('open');
-    }
 
     return(
         <>
@@ -76,25 +101,41 @@ export default function Header() {
                     </ul>
                 </nav>
                 <div className='my'>
-                    <Link className='mypage' to='/mypage'><FaRegUser />&nbsp;MY PAGE</Link>
-                    <button type='button' className='logout' onClick={handleLogout}>LOGOUT</button>
+                    {/* 일반 회원 로그인 시 */}
+                    {/* <Link className='mypage' to='/mypage'><FaRegUser />&nbsp;MY PAGE</Link>
+                    <button type='button' className='logout' onClick={handleLogout}>LOGOUT</button> */}
+                    {/* 관리자 로그인 시 */}
+                    {/* <Link className='adminpage' to='/adminpage'><FaRegUser />&nbsp;ADMIN</Link>
+                    <button type='button' className='logout' onClick={handleLogout}>LOGOUT</button> */}
+                    {/* 비로그인 시 */}
+                    <Link className='login' to='/login'><LuLogIn  />&nbsp;LOGIN</Link>
+                    <Link className='join' to='/join'>&nbsp;SIGNUP</Link>
                 </div>
             </div>
             <button className='menubar' onClick={menubarOpen}>
                 <SlMenu />
             </button>
+            <div className='menubar_bg' />
             <div className='menubar_list'>
                 <div className='my_info'>
                     <div className='profile'>
-                        <div className='profile_img' />
-                        <div className='name'>닉네임 님</div>
+                        {/* 회원 로그인 시에만 */}
+                        {/* <div className='profile_img' />
+                        <div className='name'>닉네임 님</div> */}
+                        {/* 관리자 로그인 시 */}
+                        {/* <div className='name'>ADMIN</div> */}
+                        {/* 비로그인 시 */}
+                        <div className='non_login'>
+                            <p>로그인 후 서비스를 이용해보세요</p>
+                        </div>
                     </div>
                     <div className='close_btn'>
                         <button type='button' className='close' onClick={menubarClose}><TfiClose /></button>
                     </div>
                 </div>
                 <div className='mypage_list'>
-                    <ul>
+                    {/* 회원 로그인 시 */}
+                    {/* <ul>
                         <li><Link className='menu' to='/mypage'>예약 정보</Link></li>
                         <li><Link className='menu' to='/mypage'>보유 쿠폰</Link></li>
                         <li><Link className='menu' to='/mypage'>관심 스테이</Link></li>
@@ -103,7 +144,21 @@ export default function Header() {
                     </ul>
                     <div className='logout_area'>
                         <button type='button' className='logout' onClick={handleLogout}>로그아웃</button>
+                    </div> */}
+                    {/* 관리자 로그인 시 */}
+                    {/* <ul>
+                        <li><Link className='menu' to='/mypage'>회원 관리</Link></li>
+                        <li><Link className='menu' to='/mypage'>1:1 문의글 관리</Link></li>
+                    </ul>
+                    <div className='logout_area'>
+                        <button type='button' className='logout' onClick={handleLogout}>로그아웃</button>
                     </div>
+                    */}
+                    {/* 비로그인 시 */}
+                    <ul>
+                        <li><Link className='menu' to='/login'>로그인</Link></li>
+                        <li><Link className='menu' to='/signup'>회원가입</Link></li>
+                    </ul>
                 </div>
             </div>
         </div>
