ca521cdd9ec72201f7e312193bd747dfede71e15
jeongyumin <onewmean@gmail.com>
2023-12-02 11:29:22 +0900

    [ Move ] ConfirmModal 모달 common 컴포넌트 재구성
    - ConfirmModal.jsx
      기존 Modal.jsx common 컴포넌트로 코드 수정 및 재구성
    
    - _confirmmodal.scss, main.scss
      common 컴포넌트로 이동으로 인한 모달 scss 생성
      mixin 경로 수정
    
    - _footer.scss
      z-index 추가

--- src/components/common/ConfirmModal.jsx
+++ src/components/common/ConfirmModal.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Modal({isModalClose, isModalLogin}){
+export default function ConfirmModal({handleModal, handleConfirm, noti_1, noti_2, btnText}){
 
   const handleModalBackground = (e) => {
-    isModalClose();
+    handleModal();
     e.stopPropagation();
   }
 
@@ -12,17 +12,16 @@ export default function Modal({isModalClose, isModalLogin}){
       <div className='modal_content'>
         <img src='/assets/images/logo_color.png' alt='onstayhouse 로고 이미지' />
         <div className='text_wrap'>
-          <p>로그인이 필요한 서비스입니다</p>
-          <p>로그인 창으로 이동하시겠습니까?</p>
+          <p>{noti_1}</p>
+          <p>{noti_2}</p>
         </div>
         <div className='btn_wrap'>
           <button type='button' className='close_btn'
-                  onClick={isModalClose}>닫기</button>
+                  onClick={handleModal}>닫기</button>
           <button type='button' className='login_btn'
-                  onClick={isModalLogin}>로그인하러 가기</button>
+                  onClick={handleConfirm}>{btnText || '확인'}</button>
         </div>
       </div>
     </div>    
   );
-}
-
+};
