4b825e05c61fffac29a772f02b8c0039ee3921fb
jeongyumin <onewmean@gmail.com>
2023-12-15 11:35:59 +0900

    [ Add ] roomdetail 페이지 리뷰 section( 페이지네이션, 모달, 반응형 )
    - Review.jsx
      리뷰 등록 여부 기능 구현에 따른 axios 요청 로직 수정 및 추가
      ( 등록 여부 기준 :
        로그인 회원
        회원 && 해당 숙소 예약 내역 o,
        회원 && 해당 숙소 이용 여러번일 경우의 수 고려한 리뷰 등록 x
        숙소 체크아웃날로부터 한달 이내 리뷰 등록 가능 기준 )
    
      리뷰 없을 경우 보여질 ui 구현
    
      리뷰 페이지네이션 구현 (react-js-pagination)
    
    - ReviewModal.jsx
      리뷰 등록 가능한 이용 날짜 데이터 수에 맞춘 select 구현
      ( 부모 Review 컴포넌트 registerData 데이터 활용 )
    
      handleClickClose
      : 리뷰 등록 모달창 닫기 버튼 핸들링
    
    - ReservationGuide.jsx
      반려동물 가능 여부에 따른 아이콘 추가
    
    - _roomdetail.scss
      리뷰 section 페이지네이션 및 반응형 구현
      모달 반응형 수정

--- src/components/roomdetail/Review.jsx
+++ src/components/roomdetail/Review.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { FaFileCirclePlus } from 'react-icons/fa6';
+import { TbRectangularPrismPlus } from 'react-icons/tb';
+import PagiNation from 'react-js-pagination';
 import axios from 'axios';
 import ReviewStar from './ReviewStar';
 import ReviewModal from './reviewmodal/ReviewModal';
@@ -7,45 +8,55 @@ import useUserInfo from '../../util/useUserInfo';
 
 export default function Review({roomid}) {
   const [ reviewData, setReviewData ] = useState([]);
-  const [ reservationCnt, setReservationCnt ] = useState([]);
+  const [ registerData, setRegisterData ] = useState([]);
   const [ reviewModal, setReviewModal ] = useState(false);
+  const [ currentPage, setCurrentPage ] = useState(1);
   const userInfo = useUserInfo();
   const userid = userInfo.user_id;
 
   useEffect(() => {
-    // 객실에 해당하는 리뷰 리스트 조회
-    axios.get(`http://localhost:8000/room/review/${roomid}`)
+    // 페이지네이션 요청할 리뷰 리스트 요청 : 작성일 최신순
+    axios.get(`http://localhost:8000/room/review/${roomid}/${currentPage}`)
     .then(result => {
-      setReviewData(result.data);
+        setReviewData(result.data);
     })
     .catch(error => console.log(error));
 
-    // 예약 리스트에 회원 && 객실 리스트 조회
+    // 리뷰 가능 여부 요청
     axios.get(`http://localhost:8000/room/${roomid}/${userid}`)
     .then(result => {
-      setReservationCnt(result.data.cnt);
+      if (result.data.message === '예약 x') {
+        setRegisterData([]);
+      } else {
+        setRegisterData(result.data);
+      }
     })
     .catch(error => console.log(error));
-  }, [roomid, userid]);
-  
+  }, [roomid, userid, currentPage]);
 
-  // 리뷰 내역에서 해당하는 아이디가 있는지 체크 : some으로 고칠지 고민 중
-  const matchReview = reviewData.find(review => review.user_id === userInfo.user_id);
 
+  // 리뷰 등록하기 버튼 클릭 핸들링
   const handleClickRegister = () => {
     setReviewModal(true);
   };
 
-  return(
+  // 페이지네이션 페이지 클릭 핸들링
+  const handlePageChange = (pageClickNum) => {
+    setCurrentPage(pageClickNum);
+  }
+
+  // 등록된 예약 내역이 있을 경우 보여지는 화면
+  const reviewContainer = (
     <div className='review_container'>
-      { (reservationCnt && matchReview && userid) ? // 등록하기 나오도록 테스트용
-      // { (reservationCnt && !matchReview && userid ) ?
+      { ( registerData.length > 0 && userid ) ?
         <div className='register_wrap'>
           <div className='btn_wrap'>
-            <FaFileCirclePlus />
-            <button className='register_btn' onClick={handleClickRegister}>등록하기</button>
+            <TbRectangularPrismPlus />
+            <button className='register_btn'
+                    type='button'
+                    onClick={handleClickRegister}>등록하기</button>
           </div>
-          { reviewModal && <ReviewModal /> }
+          { reviewModal && <ReviewModal setReviewModal={setReviewModal} registerData={registerData} /> }
         </div>
         : null
       }
@@ -55,7 +66,7 @@ export default function Review({roomid}) {
             <div className='review_content'>
               <img src={`/assets/images/room/${review.review_img}`} alt='숙소 이미지' />
               <div className='star_info'>
-                <ReviewStar rating={review.review_star}/>
+                <ReviewStar rating={review.review_star} />
                 <span>{review.review_star}</span>
               </div>
               <div className='review_info'>
@@ -70,9 +81,30 @@ export default function Review({roomid}) {
           ))
         }
       </div>
+      <PagiNation
+        activePage={currentPage}
+        itemsCountPerPage={4}
+        totalItemsCount={reviewData.length > 0 ? reviewData[0].total_cnt : 0}
+        pageRangeDisplayed={4}
+        onChange={handlePageChange}
+        prevPageText='<'
+        nextPageText='>'
+      />
     </div>
   );
-}
 
+  // 등록된 예약 내역이 없을 경우 보여지는 화면
+  const emptyReview = (
+    <div className='review_container'>
+      <div className='content_wrap'>
+        <img src='/assets/images/logo_gray.png' alt='onstayhouse 로고 이미지' />
+        <p>등록된 리뷰가 없습니다.</p>
+        <p>숙소 이용 후 리뷰를 남겨보세요 :)</p>
+      </div>
+    </div>
+  );
+
+  return reviewData.length > 0 ? reviewContainer : emptyReview
+}
 
 
--- src/components/roomdetail/reviewmodal/ReviewModal.jsx
+++ src/components/roomdetail/reviewmodal/ReviewModal.jsx
@@ -1,21 +1,28 @@
 import { useRef, useState } from 'react';
 import { BsCameraFill } from "react-icons/bs";
-import ModalStar from './ModalStar';
 import RegisterStar from './RegisterStar';
 
-export default function ReviewModal() {
+export default function ReviewModal({setReviewModal, registerData}) {
   const [ clickRating, setClickRating ] = useState(0);
   const fileInputRef = useRef(null);
   const [ contentText, setContentText ] = useState({content : ''});
   
+  // 별 클릭 핸들링
   const handleClickRating = (value) => {
     setClickRating(value);
   }
 
+  // 리뷰 등록 모달창 닫기 버튼 핸들링
+  const handleClickClose = () => {
+    setReviewModal(false);
+  }
+
+  // 이미지 파일 아이콘 클릭 핸들링
   const handleClickFile = () => {
     fileInputRef.current && fileInputRef.current.click();
   }
 
+  // content 입력 핸들링
   const handleChangeText = (e) => {
     setContentText({...contentText, [e.target.name] : e.target.value})
   }
@@ -27,6 +34,16 @@ export default function ReviewModal() {
           <img src='/assets/images/logo_white.png' alt='onstayhouse 로고 이미지' />
         </div>
         <form className='modal_form'>
+          <div className='date_wrap'>
+            <label htmlFor='reservation_date'>Date</label>
+            <select name='reservation_date' id='reservation_date'>
+            {registerData.map(list => 
+              <option value={`${list.checkin}, ${list.checkout}`}>
+                {`${new Date(list.checkin).toISOString().split('T')[0]} ~ ${new Date(list.checkout).toISOString().split('T')[0]} ( D -${list.remaining_days}일 )`}
+              </option>
+            )}
+            </select>
+          </div>
           <div className="img_wrap">
             <label htmlFor='file'>File</label>
             <input 
@@ -49,11 +66,10 @@ export default function ReviewModal() {
             <p>Rating</p>
             <div className='star_content'>
               <RegisterStar setClickRating={setClickRating} 
-                            handleClickRating={handleClickRating}/>
+                            handleClickRating={handleClickRating} />
               <span>{clickRating}점</span>
             </div>
           </div>
-          {/* <ModalStar setClickRating={setClickRating} /> */}
           <div className="content_wrap">
             <label htmlFor='content'>Content</label>
             <textarea 
@@ -68,7 +84,9 @@ export default function ReviewModal() {
             <span>500</span>
           </div>
           <div className='btn_wrap'>
-            <button className='close_btn' type='button'>닫기 ｘ</button>
+            <button className='close_btn' 
+                    type='button'
+                    onClick={handleClickClose}>닫기 ｘ</button>
             <button className='insert_btn'>등록 완료</button>
           </div>
         </form>
