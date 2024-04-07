5a1e6064b710bf4d7f58e766c641b043ab8cf4f0
DESKTOP-10D8ONP\rlawh <d>
2024-04-08 08:39:35 +0900

    [Refactor] 카카오 맵 호출 리팩토링
    기존 코드는 accid가 변경될 때마다 데이터를
    가져오고 이를 기반으로 카카오지도를 생성하였으나,
    수정한 코드에선 accMap이 변경될 때마다 지도를
    생성합니다. 이렇게 함으로써 데이터가 변경될 때만
    지도를 다시 생성하므로 성능을 최적화하였습니다.

--- src/components/accdetail/AccMap.jsx
+++ src/components/accdetail/AccMap.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const { kakao } = window;
 const apiBaseUrl = process.env.REACT_APP_BACKEND_ORIGIN;
@@ -10,59 +10,66 @@ export function AccMap() {
   const { accid } = useParams();
   const [map, setMap] = useState(null); // 상태로 map 객체 추가
 
-
   useEffect(() => {
     axios
       .get(`${apiBaseUrl}/findstay/acc/${accid}/map`)
       .then((result) => {
         setAccMap(result.data);
-
-        const container = document.getElementById('map');
-        const options = {
-          center: new kakao.maps.LatLng(result.data.latitude, result.data.longitude),
-          level: 3,
-        };
-
-        // Set the map to the state
-        const newMap = new kakao.maps.Map(container, options);
-        setMap(newMap);
-
-        newMap.setDraggable(false);
-        newMap.setZoomable(false);
-
-        const markerPosition = new kakao.maps.LatLng(result.data.latitude, result.data.longitude);
-        const marker = new kakao.maps.Marker({
-          position: markerPosition,
-        });
-        marker.setMap(newMap);
-
-        kakao.maps.event.addListener(newMap, 'idle', function () {
-          newMap.setCenter(markerPosition);
-        });
-
-        // Add ZoomControl after the map is created
-        const zoomControl = new kakao.maps.ZoomControl();
-        newMap.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
       })
       .catch((error) => console.log(error));
-  }, [accid]); // accid가 변경될 때마다 useEffect가 다시 실행됨
+  }, [accid]); // accid가 변경될 때마다 useEffect가 실행됨
 
-  return(
-      <div className='map_container'>
-          <div className='explain'>{accMap.acc_name}의 위치는 {accMap.address}입니다.</div>
-          <div className='map_text_container'>
-              <div className='mobile_title_container'>
-                <div className='text_title'>HELLO</div>
-                <div className='text_name'>{accMap.acc_name}</div>
-              </div>
-              <div>
-                <div className='text_address'>{accMap.address}</div>
-                <div className='text_tel'>{accMap.tel}</div>
-                <div className='text_homepage'>{accMap.homepage}</div>
-              </div>
-          </div>
-        <div className='map' id='map'></div>
+  useEffect(() => {
+    if (accMap) {
+      const container = document.getElementById("map");
+      const options = {
+        center: new kakao.maps.LatLng(accMap.latitude, accMap.longitude),
+        level: 3,
+      };
+
+      // Set the map to the state
+      const newMap = new kakao.maps.Map(container, options);
+      setMap(newMap);
+
+      newMap.setDraggable(false);
+      newMap.setZoomable(false);
+
+      const markerPosition = new kakao.maps.LatLng(
+        accMap.latitude,
+        accMap.longitude
+      );
+      const marker = new kakao.maps.Marker({
+        position: markerPosition,
+      });
+      marker.setMap(newMap);
+
+      kakao.maps.event.addListener(newMap, "idle", function () {
+        newMap.setCenter(markerPosition);
+      });
+
+      // Add ZoomControl after the map is created
+      const zoomControl = new kakao.maps.ZoomControl();
+      newMap.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+    }
+  }, [accMap]); // accMap이 변경될 때마다 useEffect가 실행됨
+
+  return (
+    <div className="map_container">
+      <div className="explain">
+        {accMap.acc_name}의 위치는 {accMap.address}입니다.
       </div>
-    ); 
-  
+      <div className="map_text_container">
+        <div className="mobile_title_container">
+          <div className="text_title">HELLO</div>
+          <div className="text_name">{accMap.acc_name}</div>
+        </div>
+        <div>
+          <div className="text_address">{accMap.address}</div>
+          <div className="text_tel">{accMap.tel}</div>
+          <div className="text_homepage">{accMap.homepage}</div>
+        </div>
+      </div>
+      <div className="map" id="map"></div>
+    </div>
+  );
 }
--- src/components/accgallery/AccGalleryBottom.jsx
+++ src/components/accgallery/AccGalleryBottom.jsx
@@ -1,38 +1,48 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import getImgPath from '../../util/getImgPath';
+import getImgPath from "../../util/getImgPath";
 
-const apiBaseUrl = process.env.REACT_APP_BACKEND_ORIGIN; 
+const apiBaseUrl = process.env.REACT_APP_BACKEND_ORIGIN;
 
 export function AccGalleryBottom() {
-  const [bottomImages, setBottomImages] = useState(null); // 초기값을 null로 설정
+  const [bottomImages, setBottomImages] = useState(null);
   const { accid } = useParams();
 
   useEffect(() => {
-    axios.get(`${apiBaseUrl}/findstay/acc/gallery/${accid}/bottom`)
-      .then(result => {
-        // 응답이 이미지 배열인 것으로 가정합니다.
+    axios
+      .get(`${apiBaseUrl}/findstay/acc/gallery/${accid}/bottom`)
+      .then((result) => {
         setBottomImages(result.data);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }, [accid]);
 
   return (
-    <div className='gallery_whole_frame'>
-      {bottomImages && bottomImages.map((room, index) => (
-        <div className='gallery_frame' key={index}>
-          <div className='gallery_title'>
-            <div className='border'></div>
-            <div className='room_name'>{room.room_name}</div>
+    <div className="gallery_whole_frame">
+      {bottomImages &&
+        bottomImages.map((room, index) => (
+          <div className="gallery_frame" key={index}>
+            <div className="gallery_title">
+              <div className="border"></div>
+              <div className="room_name">{room.room_name}</div>
+            </div>
+            <div className="gallery_img room_img">
+              <img
+                src={getImgPath.room(room.room_img1)}
+                alt={`${room.room_name}이미지1`}
+              />
+              <img
+                src={getImgPath.room(room.room_img2)}
+                alt={`${room.room_name}이미지2`}
+              />
+              <img
+                src={getImgPath.room(room.room_img3)}
+                alt={`${room.room_name}이미지3`}
+              />
+            </div>
           </div>
-          <div className="gallery_img room_img">
-            <img src={getImgPath.room(room.room_img1)} alt={`${room.room_name}이미지1`} />
-            <img src={getImgPath.room(room.room_img2)} alt={`${room.room_name}이미지2`} />
-            <img src={getImgPath.room(room.room_img3)} alt={`${room.room_name}이미지3`} />
-          </div>        
-        </div>
-      ))}
+        ))}
     </div>
   );
-};
+}
